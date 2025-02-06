3fb1d7a532f284eba3b0c3bdc8b5cb1730f090c1
dhirajudhani <dhirajudhani870@gmail.com>
2025-02-06 22:21:49 +0530

    changed in page file

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div className="m-40">
-      <Button>Hi there</Button>
+      <Button></Button>
     </div>
   );
 }
